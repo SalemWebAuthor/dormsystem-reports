a692ab39be59ea87b7a2253e60ad3af61669c803
SalemWebAuthor <100058945+SalemWebAuthor@users.noreply.github.com>
2023-06-03 19:33:33 +0800

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+// const password = document.getElementById('adm_password')
+// const form = document.getElementById('form-half')
+// const errorElement = document.getElementById('error')
+
+// form.addEventListener('submit', (e) => {
+//     let messages = []
+
+//     if (password.value.length <= 5) {
+//         messages.push('Password must be longer than 6 characters')
+//     }
+
+//     e.preventDefault()
+// })
